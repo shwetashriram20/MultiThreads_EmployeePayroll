af3496fdae7cda31f101ba1292f6cd41fc89c733
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-09 20:55:02 +0530

    Updeted Model Class

---
+++ EmployeeModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EmployeePayrollServiceSQL
+{
+    public class EmployeePayrollOperations
+    {
+        List<EmployeeDetails> employeePayrollDetailList = new List<EmployeeDetails>();
+
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine("Employee Added: " + employeeData.EmployeeName);
+            });
+
+            Console.WriteLine(this.employeePayrollDetailList.ToString());
+        }
+
+        public void addEmployeeToPayrollWithThread(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                    this.addEmployeePayroll(employeeData);
+                    Console.WriteLine("Employee Added: " + employeeData.EmployeeName);
+                });
+                thread.Start();
+            });
+            Console.WriteLine(this.employeePayrollDetailList.Count);
+        }
+
+        public void addEmployeePayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailList.Add(emp);
+        }
+
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -2,48 +2,34 @@ using EmployeePayrollServiceSQL;
 
 namespace EmpPayrollProject
 {
-    public class Tests
+    [TestClass]
+    public class UnitTest1
     {
-        [TestClass]
-        public void Setup()
-
-        {
-            EmployeePayrollOperations employeePayrollOperations = new EmployeePayrollOperations();
-        }
-
         [TestMethod]
-        public void GivenEmployeeList_InAddingEmployeeMethod_ReturnSameListThatisAdded()
+        public void Given10Employee_WhenAddedToList_ShouldMaatchEmployeeEntries()
         {
+            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 1, EmployeeName: "Viraj", PhoneNumber: "9999999999", Address: "Panvel", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Panvel", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 2, EmployeeName: "Vaibhav", PhoneNumber: "9999999999", Address: "Calgary", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Calgary", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 3, EmployeeName: "Varad", PhoneNumber: "9999999999", Address: "Alibag", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Alibag", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 4, EmployeeName: "Mayuri", PhoneNumber: "9999999999", Address: "Sagaon", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Sagaon", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 5, EmployeeName: "Mitali", PhoneNumber: "9999999999", Address: "Karjat", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Karjat", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 6, EmployeeName: "Ram", PhoneNumber: "9999999999", Address: "Neral", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Neral", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 7, EmployeeName: "Shyam", PhoneNumber: "9999999999", Address: "Uran", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Uran", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 8, EmployeeName: "Raju", PhoneNumber: "9999999999", Address: "Shelu", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Shelu", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 9, EmployeeName: "Babu", PhoneNumber: "9999999999", Address: "Pali", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Pali", Country: "India"));
+            employeeDetails.Add(new EmployeeDetails(EmployeeID: 10, EmployeeName: "Pratik", PhoneNumber: "9999999999", Address: "Mahad", Department: "Engineer", Gender: 'M', BasicPay: 10000, Deductions: 200, TaxablePay: 2000, Tax: 1000, NetPay: 6800, City: "Mahad", Country: "India"));
+
             EmployeePayrollOperations employeePayrollOperations = new EmployeePayrollOperations();
-            List<EmployeePayroll> employeesListToBeAddedToDB = new List<EmployeePayroll>();
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Vaibhav", salary = 30000, startdate = DateTime.Now, phone = 9764510067, address = "Nagpur", BasicPay = 30000, Deductions = 2000, TaxablePay = 1000, IncomeTax = 500, NetPay = 21500, DepartmentID = 2 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Sanjay", salary = 25000, startdate = DateTime.Now, phone = 9827786288, address = "Gondia", BasicPay = 22000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 20000, DepartmentID = 1 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Shubham", salary = 40000, startdate = DateTime.Now, phone = 86429532174, address = "Dharwad", BasicPay = 25000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 23000, DepartmentID = 1 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Minu", salary = 28000, startdate = DateTime.Now, phone = 7485321696, address = "Sagar", BasicPay = 18000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 17000, DepartmentID = 2 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Sai", salary = 40000, startdate = DateTime.Now, phone = 9652385137, address = "Kumta", BasicPay = 35000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 33000, DepartmentID = 2 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Akash", salary = 20000, startdate = DateTime.Now, phone = 8863259674, address = "Chitradurga", BasicPay = 20000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 18000, DepartmentID = 3 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Terissa", salary = 22000, startdate = DateTime.Now, phone = 7958623485, address = "Mirjan", BasicPay = 26000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 25000, DepartmentID = 3 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Srinivas", salary = 27000, startdate = DateTime.Now, phone = 9647853215, address = "Gokarna", BasicPay = 28000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 26000, DepartmentID = 1 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Manjesh", salary = 30000, startdate = DateTime.Now, phone = 9674851236, address = "Pavagada", BasicPay = 29000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 28000, DepartmentID = 1 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Suhas", salary = 38000, startdate = DateTime.Now, phone = 7648652952, address = "Shivamoga", BasicPay = 36000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 35000, DepartmentID = 2 });
-            employeesListToBeAddedToDB.Add(new EmployeePayroll { name = "Navnath", salary = 35000, startdate = DateTime.Now, phone = 8899775684, address = "Udupi", BasicPay = 33000, Deductions = 1000, TaxablePay = 500, IncomeTax = 500, NetPay = 31000, DepartmentID = 1 });
-
             DateTime startDateTime = DateTime.Now;
-            Console.WriteLine($"Data Addition to DB started on {startDateTime}");
-            employeePayrollOperations.AddEmployeeToPayroll(employeesListToBeAddedToDB);
+            employeePayrollOperations.addEmployeeToPayroll(employeeDetails);
+            DateTime stopDateTime = DateTime.Now;
+            Console.WriteLine("Duration without thread: " + (stopDateTime - startDateTime));
 
-            DateTime endDateTime = DateTime.Now;
-            Console.WriteLine($"Data Addition to DB ended on {endDateTime}");
-            Console.WriteLine($"Time for Adding Data to DB is {endDateTime - startDateTime}");
-            Assert.Pass();
-
-            //UC2-
-            DateTime startDateTime1 = DateTime.Now;
-            Console.WriteLine($"Data Addition to DB started on {startDateTime1}");
-            employeePayrollOperations.AddEmployeeToPayrollWithThread(employeesListToBeAddedToDB);
-            DateTime endDateTime1 = DateTime.Now;
-            Console.WriteLine($"Data Addition to DB ended on {endDateTime1}");
-            Console.WriteLine($"Time for Adding Data to DB is {endDateTime1 - startDateTime1}");
+            DateTime startDateTimeThread = DateTime.Now;
+            employeePayrollOperations.addEmployeeToPayrollWithThread(employeeDetails);
+            DateTime stopDateTimeThread = DateTime.Now;
+            Console.WriteLine("Duration with Thread: " + (stopDateTimeThread - startDateTimeThread));
 
 
         }
